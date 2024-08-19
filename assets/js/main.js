f8266fc6769d5aca267b20ff3c0ad533c64d59dd
Bashirr <beshirr096@gmail.com>
2024-08-19 14:39:28 +0400

    first commit, navbar, banner and services section completed

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+feather.replace();
+
+
+var swiper = new Swiper(".servicesSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
