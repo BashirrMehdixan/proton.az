cb5ee0a791bbc02564d1b14d2b278856f8ed8201
Bashirr <beshirr096@gmail.com>
2024-08-20 13:02:51 +0400

    partners section completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,58 @@
 feather.replace();
 
 let servicesSwiper = new Swiper(".servicesSwiper", {
-  slidesPerView: 3,
-  loop: true,
+  slidesPerView: 1,
   spaceBetween: 10,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+});
+
+let homeProjectsSwiper = new Swiper(".homeProjectsSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  loop: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
 
-let homeProjectsSwiper = new Swiper(".homeProjectsSwiper", {
-  slidesPerView: 3,
-  loop: true,
+let partnersSwiper = new Swiper(".partnersSwiper", {
+  slidesPerView: 2,
   spaceBetween: 30,
   loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  autoplay: {
+    delay: 2500,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 6,
+    },
   },
 });
